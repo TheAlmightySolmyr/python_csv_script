51da577242413019f3e34eb7bd117c94dbb36fbc
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-11-30 19:22:40 +0300

    made report generator class

--- csv_script/report_generator/generator.py
+++ csv_script/report_generator/generator.py
@@ -14,4 +14,21 @@ class ReportGenerator():
             raise ValueError(f'Unknown report type {report_type}. Available {list(available_reports.keys())}')
 
     def get_perfomance_report(self):
-        pass
+        position_data = {}
+
+        for row in self.data:
+            position = row['position']
+            performance = float(row['performance'])
+            
+            if position not in position_data:
+                position_data[position] = []
+            position_data[position].append(performance)
+
+        result = []
+
+        for position, performances in position_data.items():
+            average_performance = sum(performances) / len(performances)
+            result.append({'position': position, 
+                           'average_performance': average_performance})
+        
+        return result
