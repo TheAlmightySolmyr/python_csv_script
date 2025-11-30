591aa0147c87ebbf9883deeb7115c4214e405702
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-11-30 19:45:58 +0300

    finish line

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from csv_script.cli import cli_main
+from csv_script.csv_decomposer import read_csv
+from csv_script.report_generator.generator import ReportGenerator
 
 
 def main():
     args = cli_main()
-    return args
+    read = read_csv(args.files)
+    report = ReportGenerator(read).choose_rep_type(args.report)
+    print (report)
+
 
 
 if __name__ == '__main__':
