2c97ad4eadb0974e970ff6fb0cf6af02ba3b8bd2
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-12-01 01:59:46 +0300

    added validation into cli and markdown for README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python_csv_script
+
+[![asciicast](https://asciinema.org/a/KNCDqVdVjAVWmRaX750aTuawt.svg)](https://asciinema.org/a/KNCDqVdVjAVWmRaX750aTuawt)
+
--- csv_script/cli.py
+++ csv_script/cli.py
@@ -1,5 +1,5 @@
 import argparse
-
+import os
 from tabulate import tabulate
 
 from csv_script.csv_decomposer import read_csv
@@ -7,6 +7,13 @@ from csv_script.report_generator.generator import ReportGenerator
 
 CHOICES = ['performance']
 
+def validate_files(file_paths):
+    for file_path in file_paths:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File '{file_path}' not found")
+        if not os.path.isfile(file_path):
+            raise ValueError(f"'{file_path}' is not a file")
+    return True
 
 def cli_main():
     parser = argparse.ArgumentParser(description='Report generator')
@@ -16,6 +23,7 @@ def cli_main():
                         choices=CHOICES,
                         help='metric of the report')
     args = parser.parse_args()
+    validate_files(args.files)
     read = read_csv(args.files)
     report = ReportGenerator(read).choose_rep_type(args.report)
     print(tabulate(report, headers='keys', showindex=range(1, len(report) + 1)))
