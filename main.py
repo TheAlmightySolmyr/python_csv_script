a300c8af868733ec969707fe3346c40ab4b615f5
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-11-30 19:54:07 +0300

    adde tabulate to main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from csv_script.cli import cli_main
 from csv_script.csv_decomposer import read_csv
 from csv_script.report_generator.generator import ReportGenerator
+from tabulate import tabulate
 
 
 def main():
     args = cli_main()
     read = read_csv(args.files)
     report = ReportGenerator(read).choose_rep_type(args.report)
-    print (report)
+    print(tabulate(report))
 
 
 
