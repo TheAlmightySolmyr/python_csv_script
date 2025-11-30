d9f9f65e320f7468f7c2c38b488f629486ad3318
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-11-30 18:09:55 +0300

    made choose_rep_type

--- csv_script/report_generator/generator.py
+++ csv_script/report_generator/generator.py
@@ -3,8 +3,15 @@ class ReportGenerator():
     def __init__(self, data):
         self.data = data
 
-    def choose_rep_type(self):
-        pass
+    def choose_rep_type(self, report_type):
+        available_reports = {'performance': 'get_perfomance_report'}
+
+        if report_type in available_reports:
+            method_name = available_reports[report_type]
+            method = getattr(self, method_name)
+            return method()
+        else:
+            raise ValueError(f'Unknown report type {report_type}. Available {list(available_reports.keys())}')
 
     def get_perfomance_report(self):
         pass
