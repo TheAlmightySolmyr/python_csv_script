a6bd77557dcaa099dd8a4cd28afd7de415f34589
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-12-01 01:40:15 +0300

    added validation for csv_decomposer

--- csv_script/csv_decomposer.py
+++ csv_script/csv_decomposer.py
@@ -4,7 +4,10 @@ import csv
 def read_csv(file_paths: list[str]):
     result = []
     for path in file_paths:
-        with open(path) as file:
-            dict_csv = csv.DictReader(file)
-            result.extend(list(dict_csv))
+        try:
+            with open(path) as file:
+                dict_csv = csv.DictReader(file)
+                result.extend(list(dict_csv))
+        except csv.Error as e:
+            raise ValueError(f'Error reading CSV file {path}: {e}')
     return result
